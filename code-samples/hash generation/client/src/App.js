2101a91efec09581b193caf58186f3cfd7c2d8bd
panchaltej <tejaspanchal49@gmail.com>
2018-11-18 02:51:21 -0800

    Added hash-generation sample

---
+++ code-samples/hash generation/client/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import Main from './components/Main'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Hash Generator</h1>
+        </header>
+        <p className="App-intro">
+          Upload file to generate SHA-256
+        </p>
+        <div>
+          <Main/>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
