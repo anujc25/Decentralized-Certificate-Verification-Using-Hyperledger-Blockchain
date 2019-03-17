d8f1e6bb9d8c22e84d846bfadfd3d5b2ccb51d55
panchaltej <tejaspanchal49@gmail.com>
2019-03-17 11:11:33 -0700

    Updated chaincode with new diploma struct and composite key

--- chaincode/vericert/vericert.go
+++ chaincode/vericert/vericert.go
@@ -5,6 +5,7 @@ package main
  * 2 specific Hyperledger Fabric specific libraries for Smart Contracts
  */
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 
@@ -13,15 +14,23 @@ import (
 	sc "github.com/hyperledger/fabric/protos/peer"
 )
 
+const UNIVERSITY_ROLE = "UNIVERSITY"
+
 // SmartContract Define the Smart Contract structure
 type SmartContract struct {
 }
 
-// CertificateMetadata Define the car structure, with 4 properties.  Structure tags are used by encoding/json library
-type CertificateMetadata struct {
-	Timestamp  string `json:"timestamp"`
-	Issuer     string `json:"issuer"`
-	UploadedBy string `json:"uploadedBy"`
+// DiplomaMetadata Defines the diploma structure, with 9 properties.  Structure tags are used by encoding/json library
+type DiplomaMetadata struct {
+	Timestamp   string `json:"timestamp"`
+	DiplomaUUID string `json:"diplomaUUID"`
+	Issuer      string `json:"issuer"`
+	Term        string `json:"term"`
+	Degree      string `json:"degree"`
+	Department  string `json:"department"`
+	Name        string `json:"name"`
+	EmailId     string `json:"emailId"`
+	IpfsLink    string `json:"ipfsLink"`
 }
 
 /*
@@ -41,14 +50,16 @@ func (s *SmartContract) Invoke(APIstub shim.ChaincodeStubInterface) sc.Response 
 	// Retrieve the requested Smart Contract function and arguments
 	function, args := APIstub.GetFunctionAndParameters()
 	// Route to the appropriate handler function to interact with the ledger appropriately
-	if function == "queryCertificate" {
-		return s.queryCertificate(APIstub, args)
+	if function == "queryDiploma" {
+		return s.queryDiploma(APIstub, args)
 	} else if function == "initLedger" {
 		return s.initLedger(APIstub)
-	} else if function == "addCertificate" {
-		return s.addCertificate(APIstub, args)
-	} else if function == "updateCertificate" {
-		return s.updateCertificate(APIstub, args)
+	} else if function == "addDiploma" {
+		return s.addDiploma(APIstub, args)
+	} else if function == "updateDiploma" {
+		return s.updateDiploma(APIstub, args)
+	} else if function == "queryDiplomaByIssuer" {
+		return s.queryDiplomaByIssuer(APIstub, args)
 	} else if function == "fetchUserRole" {
 		return s.fetchUserRole(APIstub)
 	}
@@ -56,50 +67,180 @@ func (s *SmartContract) Invoke(APIstub shim.ChaincodeStubInterface) sc.Response 
 	return shim.Error("Invalid Smart Contract function name.")
 }
 
-func (s *SmartContract) queryCertificate(APIstub shim.ChaincodeStubInterface, args []string) sc.Response {
+// Expected args[]
+// 	   0
+// [diplomaUUID]
+func (s *SmartContract) queryDiploma(APIstub shim.ChaincodeStubInterface, args []string) sc.Response {
 
 	if len(args) != 1 {
 		return shim.Error("Incorrect number of arguments. Expecting 1")
 	}
 
-	certificateMetadataAsBytes, err := APIstub.GetState(args[0])
-	if certificateMetadataAsBytes == nil {
-		return shim.Error("No certificate found!")
+	diplomaMetadataAsBytes, err := APIstub.GetState(args[0])
+	if diplomaMetadataAsBytes == nil {
+		return shim.Error("No Diploma found for given ID!")
 	}
 	if err != nil {
 		return shim.Error(err.Error())
 	}
 
-	return shim.Success(certificateMetadataAsBytes)
+	return shim.Success(diplomaMetadataAsBytes)
 }
 
 func (s *SmartContract) initLedger(APIstub shim.ChaincodeStubInterface) sc.Response {
 	return shim.Success(nil)
 }
 
-func (s *SmartContract) addCertificate(APIstub shim.ChaincodeStubInterface, args []string) sc.Response {
+// Expected args[]
+// 		0		1		2		3	  4		5	   6	7		8
+// [timestamp, UUID, issuer, term, degree, dept, name, email, ipfslink]
+func (s *SmartContract) addDiploma(APIstub shim.ChaincodeStubInterface, args []string) sc.Response {
 
-	if len(args) != 4 {
-		return shim.Error("Incorrect number of arguments. Expecting 4")
+	fmt.Println("- start add diploma")
+	// RBAC check
+	value, _, err := cid.GetAttributeValue(APIstub, "role")
+	if err != nil {
+		errResponse := "{\"error\": " + err.Error() + "\"}"
+		return shim.Error(errResponse)
+	}
+	if value != UNIVERSITY_ROLE {
+		errResponse := "{\"error\": " + "Current role" + value + "is unauthorized for the transaction" + "\"}"
+		return shim.Error(errResponse)
 	}
 
-	var certificateHash = args[0]
-	var certificateMetadata = CertificateMetadata{Timestamp: args[1], Issuer: args[2], UploadedBy: args[3]}
+	if len(args) != 9 {
+		errResponse := "{\"error\": " + "Incorrect number of arguments. Expecting 9" + "\"}"
+		return shim.Error(errResponse)
+	}
 
-	certificateMetadataAsBytes, err := json.Marshal(certificateMetadata)
+	// Upload diploma transaction
+	var diplomaUUID = args[1]
+	var diplomaMetadata = DiplomaMetadata{Timestamp: args[0], DiplomaUUID: args[1], Issuer: args[2], Term: args[3], Degree: args[4], Department: args[5], Name: args[6], EmailId: args[7], IpfsLink: args[8]}
+
+	diplomaMetadataAsBytes, err := json.Marshal(diplomaMetadata)
 	if err != nil {
 		return shim.Error(err.Error())
 	}
 
-	err = APIstub.PutState(certificateHash, certificateMetadataAsBytes)
+	err = APIstub.PutState(diplomaUUID, diplomaMetadataAsBytes)
 	if err != nil {
 		return shim.Error(err.Error())
 	}
 
+	// Indexing diploma for different query
+	// Issuer - UUID index to query all the diploma uploaded by a issuer
+	indexName := "issuer~uuid"
+	issuerUuidIndexKey, err := APIstub.CreateCompositeKey(indexName, []string{diplomaMetadata.Issuer, diplomaMetadata.DiplomaUUID})
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	//  Save index entry to state. Only the key name is needed, no need to store a duplicate copy of the marble.
+	//  Note - passing a 'nil' value will effectively delete the key from state, therefore we pass null character as value
+	value := []byte{0x00}
+	APIstub.PutState(issuerUuidIndexKey, value)
+
+	// ==== Diploma saved and indexed. Return success ====
+	fmt.Println("- end add diploma")
 	return shim.Success(nil)
 }
 
-func (s *SmartContract) updateCertificate(APIstub shim.ChaincodeStubInterface, args []string) sc.Response {
+func (s *SmartContract) updateDiploma(APIstub shim.ChaincodeStubInterface, args []string) sc.Response {
+	return shim.Success(nil)
+}
+
+// ===========================================================================================
+// constructQueryResponseFromIterator constructs a JSON array containing query results from
+// a given result iterator
+// ===========================================================================================
+func constructQueryResponseFromIterator(resultsIterator shim.StateQueryIteratorInterface) (*bytes.Buffer, error) {
+	// buffer is a JSON array containing QueryResults
+	var buffer bytes.Buffer
+	buffer.WriteString("[")
+
+	bArrayMemberAlreadyWritten := false
+	for resultsIterator.HasNext() {
+		queryResponse, err := resultsIterator.Next()
+		if err != nil {
+			return nil, err
+		}
+		// Add a comma before array members, suppress it for the first array member
+		if bArrayMemberAlreadyWritten == true {
+			buffer.WriteString(",")
+		}
+		buffer.WriteString("{\"Key\":")
+		buffer.WriteString("\"")
+		buffer.WriteString(queryResponse.Key)
+		buffer.WriteString("\"")
+
+		buffer.WriteString(", \"Record\":")
+		// Record is a JSON object, so we write as-is
+		buffer.WriteString(string(queryResponse.Value))
+		buffer.WriteString("}")
+		bArrayMemberAlreadyWritten = true
+	}
+	buffer.WriteString("]")
+
+	return &buffer, nil
+}
+
+// ===========================================================================================
+// addPaginationMetadataToQueryResults adds QueryResponseMetadata, which contains pagination
+// info, to the constructed query results
+// ===========================================================================================
+func addPaginationMetadataToQueryResults(buffer *bytes.Buffer, responseMetadata *pb.QueryResponseMetadata) *bytes.Buffer {
+
+	buffer.WriteString("[{\"ResponseMetadata\":{\"RecordsCount\":")
+	buffer.WriteString("\"")
+	buffer.WriteString(fmt.Sprintf("%v", responseMetadata.FetchedRecordsCount))
+	buffer.WriteString("\"")
+	buffer.WriteString(", \"Bookmark\":")
+	buffer.WriteString("\"")
+	buffer.WriteString(responseMetadata.Bookmark)
+	buffer.WriteString("\"}}]")
+
+	return buffer
+}
+
+// Expected args[]
+// 	   0
+// [issuer]
+func (s *SmartContract) queryDiplomaByIssuer(APIstub shim.ChaincodeStubInterface, args []string) sc.Response {
+
+	// RBAC check
+	value, _, err := cid.GetAttributeValue(APIstub, "role")
+	if err != nil {
+		errResponse := "{\"error\": " + err.Error() + "\"}"
+		return shim.Error(errResponse)
+	}
+	if value != UNIVERSITY_ROLE {
+		errResponse := "{\"error\": " + "Current role" + value + "is unauthorized for the transaction" + "\"}"
+		return shim.Error(errResponse)
+	}
+
+	if len(args) != 1 {
+		return shim.Error("Incorrect number of arguments. Expecting 1")
+	}
+
+	fmt.Println("- start queryDiplomaByIssuer", args[0])
+	issuer := args[0]
+
+	// Query the Issuer~UUID index by color
+	// This will execute a key range query on all keys starting with 'Issuer'
+	issuerDiplomaResultsIterator, err := APIstub.GetStateByPartialCompositeKey("issuer~uuid", []string{issuer})
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	defer issuerDiplomaResultsIterator.Close()
+
+	buffer, err := constructQueryResponseFromIterator(issuerDiplomaResultsIterator)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	fmt.Printf("- queryDimplomaByIssuer queryResult:\n%s\n", buffer.String())
+
+	return shim.Success(buffer.Bytes())
+
 	return shim.Success(nil)
 }
 
