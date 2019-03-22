a62f04bfa9c4ffc6e601ed3f7bf1f034afa995b8
panchaltej <tejaspanchal49@gmail.com>
2019-03-22 01:29:06 -0700

    modified view all diploma table

--- client/src/components/Homepages/UniversityComponents/UniversityAllUploads.js
+++ client/src/components/Homepages/UniversityComponents/UniversityAllUploads.js
@@ -13,52 +13,52 @@ class UniversityAllUploads extends Component {
             this.setState({
                 allDiplomas : res
             })
-
         })
     }
 
     renderDiplomaInformation = () => {
         return this.state.allDiplomas.map((diploma,index)=>{
             return(
-                
-                <div class="my-3 p-3 bg-white rounded shadow-sm">
-                    <div class="media text-muted pt-3">
-                    <table class="table table-striped table-sm">
-                        <thead>
-                            <tr>
-                            <th>Degree</th>
-                            <th>department</th>
-                            <th>emailId</th>
-                            <th>name</th>
-                            <th>term</th>
-                            <th>ipfsLink</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                            <td>{diploma.degree}</td>
-                            <td>{diploma.department}</td>
-                            <td>{diploma.emailId}</td>
-                            <td>{diploma.name}</td>
-                            <td>{diploma.term}</td>
-                            <td>{diploma.ipfsLink}</td>
-                            </tr>   
-                            </tbody>
-                            </table>
-                     </div>
-        
-                </div>
+                    <tr>
+                        <td>{diploma.degree}</td>
+                        <td>{diploma.department}</td>
+                        <td>{diploma.emailId}</td>
+                        <td>{diploma.name}</td>
+                        <td>{diploma.term}</td>
+                        <td>{diploma.ipfsLink}</td>
+                    </tr>           
             );
-        });
-        
+        });      
     }
-  render () {
-    return (
-      <div className='container'>
-        {this.renderDiplomaInformation()}
-      </div>
-    )
-  }
+    
+    render () {
+        return (
+            <div className='container'>
+                <div class="my-3 p-3 bg-white rounded shadow-sm">
+                    <div class="media text-muted pt-1">
+                        All Diploma
+                    </div>
+                    <div class="media text-muted pt-3">
+                        <table class="table table-striped table-sm">
+                            <thead>
+                                <tr>
+                                    <th>Degree</th>
+                                    <th>department</th>
+                                    <th>emailId</th>
+                                    <th>name</th>
+                                    <th>term</th>
+                                    <th>ipfsLink</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {this.renderDiplomaInformation()}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default UniversityAllUploads
