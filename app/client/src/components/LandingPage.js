300d4b040e61a13bf50ae1259d77a9a5301b2822
Anuj Chaudhari <chaudharianuj93@gmail.com>
2019-04-19 16:25:31 -0700

    Add css

--- app/client/src/components/LandingPage.js
+++ app/client/src/components/LandingPage.js
@@ -1,53 +1,16 @@
 import React, { Component } from 'react'
-import {bindActionCreators} from 'redux'
-import { Route, Link, Switch, Redirect,withRouter, BrowserRouter } from 'react-router-dom'
-import '../css/cover.css'
+import { bindActionCreators } from 'redux'
+import { Route, Link, Switch, Redirect, withRouter, BrowserRouter } from 'react-router-dom'
+import '../css/1.css'
+import '../css/2.css'
+import '../css/3.css'
+import '../css/4.css'
 import * as API from './../services/loginService'
-import {SaveUser} from '../actions/actions'
-import {connect} from 'react-redux'
+import { SaveUser } from '../actions/actions'
+import { connect } from 'react-redux'
+import img1 from '../images/bg.jpg'
 
 class LandingPage extends Component {
-
-  state = {
-    loginError: '',
-    username: '',
-    secret: '' ,
-    role: 'STUDENT'   
-}
-
-  doLogin = (e) => {
-
-  var payload = {
-      username: this.state.username,
-      secret : this.state.secret,
-      role : this.state.role
-  }
-
-  API.universityLogin(payload)
-      .then((res) => {
-        console.log(res);
-        if (!res.error && res.result && res.result.role) {
-          if (res.result.role != ''){
-            var obj = {
-              userName: this.state.username,
-              role: this.state.role
-            }
-            this.props.SaveUser(obj)
-            this.props.history.push('/homepage/' + res.result.role)
-          }
-          else {
-            this.setState({
-              loginError: "Login Failed. " + res.error,
-            });
-          }
-        } else {
-          this.setState({
-            loginError: "Login Failed. " + res.error,
-          });            
-        }
-      });
-  }
-
   render () {
     return (
     // <div classNameName="container">
@@ -115,17 +78,50 @@ class LandingPage extends Component {
 
 
 
+        <div class='peers ai-s fxw-nw h-100vh'>
+          <div class='d-n@sm- peer peer-greed h-100 pos-r bgr-n bgpX-c bgpY-c bgsz-cv' style={{ 'background-image': `url(${img1})` }}>
+            <div class='pos-a centerXY'>
+              <div class='bgc-white bdrs-50p pos-r' style={{ 'width': '120px', 'height': '120px' }}>
+                <img class='pos-a centerXY' src='assets/static/images/logo.png' alt='' />
+              </div>
+            </div>
           </div>
-
+          <div class='col-12 col-md-4 peer pX-40 pY-80 h-100 bgc-white scrollable pos-r' style={{ 'min-width': '320px;' }}>
+            <h4 class='fw-300 c-grey-900 mB-40'>Login</h4>
+            <form>
+              <div class='form-group'>
+                <label class='text-normal text-dark'>Username</label>
+                <input type='email' class='form-control' placeholder='John Doe' />
+              </div>
+              <div class='form-group'>
+                <label class='text-normal text-dark'>Password</label>
+                <input type='password' class='form-control' placeholder='Password' />
+              </div>
+              <div class='form-group'>
+                <div class='peers ai-c jc-sb fxw-nw'>
+                  <div class='peer'>
+                    <div class='checkbox checkbox-circle checkbox-info peers ai-c'>
+                      <input type='checkbox' id='inputCall1' name='inputCheckboxesCall' class='peer' />
+                      <label for='inputCall1' class=' peers peer-greed js-sb ai-c'>
+                        <span class='peer peer-greed'>Remember Me</span>
+                      </label>
+                    </div>
+                  </div>
+                  <div class='peer'>
+                    <button class='btn btn-primary'>Login</button>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
         </div>
-
       </div>
     )
-    }
+  }
 }
 
-function mapDispatchToProps(dispatch){
-  return bindActionCreators({SaveUser : SaveUser}, dispatch);
+function mapDispatchToProps (dispatch) {
+  return bindActionCreators({ SaveUser: SaveUser }, dispatch)
 }
 
 export default withRouter(connect(null, mapDispatchToProps)(LandingPage))
