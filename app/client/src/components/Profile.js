dea7d565d7c103942bb01003deda8a63e5880235
Jay Desai <janhudesai@gmail.com>
2019-04-26 01:12:46 -0700

    added Profile page

---
+++ app/client/src/components/Profile.js
@@ -0,0 +1,111 @@
+import React, {Component} from 'react';
+import { withRouter } from 'react-router-dom'
+import {connect} from 'react-redux'
+
+class Profile extends Component{
+ 
+    state = {
+        oldPassword:'',
+        newPassword : '',
+        message:""
+    };
+
+    onSubmit = (e) => {
+        let payload;
+        payload = {
+            "oldPassword":this.state.oldPassword,
+            "newPassword":this.state.newPassword
+        }
+        console.log("Payload:",payload)
+        this.setState({
+            message: "Password Changed successfully",
+        }); 
+    }
+
+    render(){
+        return(
+            <div class="row gap-20 masonry pos-r">
+            <div class="masonry-item col-md-2">
+            <div>
+                    <div class="layers bd bgc-white p-15">
+                      <div class="layer w-100 mB-10">
+                        <h4 class="c-grey-900">Name</h4>
+                        <label>{this.props.userDetail.userName}</label>
+                      </div>
+                    </div>
+                  </div>
+                  <div style={{ 'width': '5px',
+                  'height': 'auto',
+                  'display': 'inline-block' }} />
+
+                  <div>
+                    <div class="layers bd bgc-white p-15">
+                      <div class="layer w-100 mB-10">
+                        <h4 class="c-grey-900">Role</h4>
+                        <label>{this.props.userDetail.role}</label>
+                      </div>
+                    </div>
+                  </div>
+             </div>  
+             
+            
+            <div class="masonry-item col-md-4">
+                <div class="bgc-white p-20 bd">
+                    <h6 class="c-grey-900">Update Password</h6>
+                    <div class="mT-30">
+                        <form onSubmit={this.onSubmit}>
+                            <div class="form-row">
+                                <div class="form-group col-md-10">
+
+                                    <label>Old Password</label>
+                                    <input type="text" className="form-control" id="oldPassword"  placeholder="Enter Old Password"
+                                                onChange={(event) => {
+                                                this.setState({
+                                                        ...this.state,
+                                                        oldPassword: event.target.value
+                                                    });
+                                                }}
+                                            >
+                                    </input>
+
+                                </div>
+                            </div>
+
+                            <div class="form-row">
+                                <div class="form-group col-md-10">
+
+                                    <label>New Password</label>
+                                    <input type="text" className="form-control" id="newPassword" placeholder="Enter New Password"
+                                        onChange={(event) => {
+                                            this.setState({
+                                                    ...this.state,
+                                                    newPassword: event.target.value
+                                                });
+                                            }}
+                                    >
+                                    </input>
+
+                                </div>
+                            </div>
+                            
+                            <button className='btn btn-primary' type="submit">Submit</button>
+                        </form>
+                        <label>{this.state.message}</label>
+                    </div>
+                </div> 
+            </div>   
+            
+        </div>
+                 
+        );
+    }
+}
+
+function mapStateToProps(state){
+    return {
+        userDetail: state.userDetail
+    }
+}
+
+export default connect(mapStateToProps)(Profile)
+
